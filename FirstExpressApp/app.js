a16da41d4fef9b774bd1839bff4d4b66439cdac1
joel <jpayan01@gmail.com>
2019-07-30 22:45:10 -0500

    Added new project for express

---
+++ FirstExpressApp/app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+// "/" => "Hi there!"
+app.get("/", function(req, res){
+    res.send("Hi there!");
+});
+// "/bye" => "Goodbye"
+app.get("/bye", function(req, res){
+    res.send("Goodbye!!!");
+});
+// "/dog" => "MEOW"
+app.get("/dog", function(req, res){
+    res.send("MEOW!!!");
+});
+
+
+app.listen(3000, function() {
+    console.log("Listening on port 3000");
+});
