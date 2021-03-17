a97183b59fd9fce25bb5dd59448ecc0c556604e2
Joel E Payan <jpayan01@gmail.com>
2021-03-16 20:34:32 -0600

    Delete APIs directory

--- APIs/first_request.js
+++
@@ -1,7 +0,0 @@
-var request = require("request");
-
-request("http://www.google.com", function(error, response, body){
-    if(!error && response.statusCode == 200){
-        console.log(body);
-    }
-});
