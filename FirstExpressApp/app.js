5e74231b45f68f96dbcd2c45adcb3b9a0d9ab9bc
Joel Payan <jpayan01@gmail.com>
2021-04-11 23:47:24 -0500

    created new routes for express

--- FirstExpressApp/app.js
+++ FirstExpressApp/app.js
@@ -1,18 +1,31 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
+// app.use(()=> {
+//     console.log('We got a new request!');
+// })
 // "/" => "Hi there!"
 app.get("/", function(req, res){
-    res.send("Hi there!");
+    res.send("Welcome to the homepage!");
 });
+
+app.get('/r/:subreddit', (req, res) =>{
+    const {subreddit} = req.params;
+    res.send(`this is a ${subreddit} subreddit.`);
+})
 // "/bye" => "Goodbye"
-app.get("/bye", function(req, res){
-    res.send("Goodbye!!!");
+app.get("/dogs", function(req, res){
+    res.send("Woof!");
 });
 // "/dog" => "MEOW"
-app.get("/dog", function(req, res){
-    res.send("MEOW!!!");
+app.get("/cats", function(req, res){
+    res.send("Meow!");
 });
+
+app.get('/search', (req, res)=> {
+    const {q} = req.query;
+    res.send(`<h2>Search results for ${q}</h2`);
+})
 
 app.listen(3000, function() {
     console.log("Listening on port 3000");
