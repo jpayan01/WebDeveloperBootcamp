ee4e41eca0c870b8659f93bf95c660292e2eccec
Joel Payan <jpayan01@gmail.com>
2021-04-21 22:39:41 -0500

    .

--- EjsDemo/app.js
+++ EjsDemo/app.js
@@ -11,37 +11,49 @@ app.set('view engine', 'ejs');
 const comments = [
     {
         username: 'Joel', 
-        comments: 'The best in test automation'
+        comment: 'The best in test automation'
     },
     {
         username: 'Marcela',
-        comments: "Hottie and my love!"
+        comment: "Hottie and my love!"
     }
 ]
 
 app.get('/comments', (req, res)=> {
     res.render('comments/index', {comments})
-})
+});
+
+app.get('/comments/new', (req, res)=>{
+    res.render('comments/new');
+});
+
+app.post('/comments', (req, res)=> {
+    
+    const {username, comment} = req.body;
+    comments.push({username, comment});
+
+    res.redirect('/comments');
+});
 
 app.get("/", (req, res)=> {
     res.render("home.ejs");
 });
 
-app.get("/fallinlovewith/:thing", (req, res)=>{
-    var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
-});
+// app.get("/fallinlovewith/:thing", (req, res)=>{
+//     var thing = req.params.thing;
+//     res.render("love.ejs", {thingVar: thing});
+// });
 
-app.get('/random', (req, res)=> {
-    const randNumber = Math.floor(Math.random() * 10) + 1; 
-    res.render('random', {rand: randNumber}); 
-});
+// app.get('/random', (req, res)=> {
+//     const randNumber = Math.floor(Math.random() * 10) + 1; 
+//     res.render('random', {rand: randNumber}); 
+// });
 
-app.get('/r/:subreddit', (req, res) => {
-    const {subreddit } = req.params;
-    res.render('subreddit', {subreddit});
+// app.get('/r/:subreddit', (req, res) => {
+//     const {subreddit } = req.params;
+//     res.render('subreddit', {subreddit});
 
-})
+// })
 
 app.listen(3000, ()=>{
     console.log("Server listening on Port: 3000");
